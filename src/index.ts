6e51aec4ad76e938eeab0cff0ca7b1547b44642d
JoaoFPessoa <joaoppessoa@uol.com.br>
2023-03-22 18:18:04 -0300

    add: created server and connected to mongoDb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-let text: string;
+import express from "express";
+import mongoose from "mongoose";
+import { router } from "./router";
 
-text = "123";
+const app = express();
 
-console.log(text);
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const port = 3001;
+
+    app.use(express.json());
+
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log("Erro ao conectar ao mongo db"));
